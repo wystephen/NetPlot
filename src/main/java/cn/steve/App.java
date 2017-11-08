53fc7a456c3408fee756918089f74ae39fadc162
steve <551619855@qq.com>
2017-11-08 20:43:04 +0800

    ~GL~

--- src/main/java/cn/steve/App.java
+++ src/main/java/cn/steve/App.java
@@ -13,23 +13,32 @@ import org.jzy3d.colors.colormaps.ColorMapRainbow;
 import org.jzy3d.javafx.JavaFXChartFactory;
 import org.jzy3d.javafx.JavaFXRenderer3d;
 import org.jzy3d.javafx.controllers.mouse.JavaFXCameraMouseController;
+import org.jzy3d.maths.Coord3d;
 import org.jzy3d.maths.Range;
 import org.jzy3d.plot3d.builder.Builder;
 import org.jzy3d.plot3d.builder.Mapper;
+import org.jzy3d.plot3d.primitives.Scatter;
 import org.jzy3d.plot3d.primitives.Shape;
 import org.jzy3d.plot3d.rendering.canvas.Quality;
+//import org.jzy3d.plot3d.primitives.*;
+
+
+import java.lang.Math.*;
+
+import static java.lang.Math.cos;
+import static java.lang.Math.sin;
 
 /**
  * Showing how to pipe an offscreen Jzy3d chart image to a JavaFX ImageView.
- *
+ * <p>
  * {@link JavaFXChartFactory} delivers dedicated  {@link JavaFXCameraMouseController}
  * and {@link JavaFXRenderer3d}
- *
+ * <p>
  * Support
  * Rotation control with left mouse button hold+drag
  * Scaling scene using mouse wheel
  * Animation (camera rotation with thread)
- *
+ * <p>
  * TODO :
  * Mouse right click shift
  * Keyboard support (rotate/shift, etc)
@@ -37,7 +46,20 @@ import org.jzy3d.plot3d.rendering.canvas.Quality;
  * @author Martin Pernollet
  */
 public class App extends Application {
+
+
+    static AWTChart global_chart;
+    static float attitude = 0.0F;
+    static AddLine al;
+
+
+
+
     public static void main(String[] args) {
+        Thread a = new Thread(new AddLine());
+        a.start();
+
+
         Application.launch(args);
     }
 
@@ -47,8 +69,8 @@ public class App extends Application {
 
         // Jzy3d
         JavaFXChartFactory factory = new JavaFXChartFactory();
-        AWTChart chart  = getDemoChart(factory, "offscreen");
-        ImageView imageView = factory.bindImageView(chart);
+        global_chart = getDemoChart(factory, "offscreen");
+        ImageView imageView = factory.bindImageView(global_chart);
 
         // JavaFX
         StackPane pane = new StackPane();
@@ -57,7 +79,7 @@ public class App extends Application {
         stage.show();
         pane.getChildren().add(imageView);
 
-        factory.addSceneSizeChangedListener(chart, scene);
+        factory.addSceneSizeChangedListener(global_chart, scene);
 
         stage.setWidth(500);
         stage.setHeight(500);
@@ -69,7 +91,7 @@ public class App extends Application {
         Mapper mapper = new Mapper() {
             @Override
             public double f(double x, double y) {
-                return x * Math.sin(x * y);
+                return x * sin(x * y);
             }
         };
 
@@ -83,6 +105,13 @@ public class App extends Application {
         surface.setFaceDisplayed(true);
         surface.setWireframeDisplayed(false);
 
+//        Scatter[] scatters = new Scatter[10];
+//
+//        for(int i=0;i<10;++i)
+//        {
+//            scatters[i] = new Scatter(new Coord3d(i,i,i),new Color(0.9F,0.2F,0.7F),1.0);
+//        }
+
         // -------------------------------
         // Create a chart
         Quality quality = Quality.Advanced;
@@ -92,6 +121,53 @@ public class App extends Application {
         // let factory bind mouse and keyboard controllers to JavaFX node
         AWTChart chart = (AWTChart) factory.newChart(quality, toolkit);
         chart.getScene().getGraph().add(surface);
+
+        Coord3d[] cs = new Coord3d[1000];
+        for (int i = 0; i < 1000; ++i) {
+            cs[i] = new Coord3d(i * 0.01F, i * 0.01F, i * 0.01F);
+        }
+
+        Scatter ts = new Scatter(cs);//,new Color(0.9F,0.2F,0.7F),2.0F);
+
+        chart.getScene().getGraph().add(ts);
+
+//        for(int i=0;i<10;++i)
+//        {
+//            Coord3d[]  cs = new Coord3d[1];
+//            cs[0].add(i,i,i);
+//            Scatter ts = new Scatter(cs);//,new Color(0.9F,0.2F,0.7F),2.0F);
+////            chart.getScene().getGraph().add(ts);
+//        }
+
         return chart;
+    }
+    static class AddLine implements Runnable {
+        public AddLine(){
+
+        }
+
+        public void run() {
+            while (true) {
+                if (global_chart!=null) {
+                    Coord3d[] cs = new Coord3d[1000];
+                    for (int i = 0; i < 1000; ++i) {
+                        cs[i] = new Coord3d(i * 0.01F * sin(attitude), i * 0.01F * cos(attitude), i * 0.01F);
+                    }
+
+                    Scatter ts = new Scatter(cs);//,new Color(0.9F,0.2F,0.7F),2.0F);
+
+                    global_chart.getScene().getGraph().add(ts);
+                    attitude += 0.01;
+//                    global_chart.getScene().getGraph().remove()
+
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
     }
 }
